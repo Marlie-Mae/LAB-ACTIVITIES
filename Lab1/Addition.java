84e59370e80f0adabd0def248922ca69ccffbbc3
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-04-23 11:18:28 +0800

    Add files via upload

---
+++ Lab1/Addition.java
@@ -0,0 +1,13 @@
+public class Addition {
+	private int num1;
+	private int num2;
+	private int sum;
+	
+	public int calculteSum(int num1, int num2) {
+		sum = num1 + num2;
+		System.out.println("The sum is " + sum);
+		
+		return sum;
+		
+	}
+}
