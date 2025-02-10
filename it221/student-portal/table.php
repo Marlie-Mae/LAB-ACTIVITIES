4cfe6d5064bf00c0b9ef7baf0caa13e9bf0eece4
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2025-02-10 23:22:44 +0800

    Update table.php

--- it221/student-portal/table.php
+++ it221/student-portal/table.php
@@ -14,42 +14,44 @@
             if ($rows > 0) {
         ?>
         <table id="student_info" class="display">
-                <thead>
-                    <tr>
-                        <th>Student No.</th>
-                        <th>Last Name</th>
-                        <th>First Name</th>
-                        <th>Middle Name</th>
-                        <th>Course</th>
-                        <th>Year Level</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        while ($data = mysqli_fetch_assoc($query)) {
-                    ?>
-                    <tr>
-                        <td><?php echo $data['student_no'];?></td>
-                        <td><?php echo $data['last_name'];?></td>
-                        <td><?php echo $data['first_name'];?></td>
-                        <td><?php echo $data['middle_name'];?></td>
-                        <td><?php echo $data['course_code'];?></td>
-                        <td><?php echo $data['year_level'];?></td>
-                        <td><a href="edit.php??student_no=<?php echo $data['student_no']; ?>"></td>
-                    </tr>
-                    <?php } ?>
-                </tbody>
+            <thead>
+                <tr>
+                    <th>Student No.</th>
+                    <th>Last Name</th>
+                    <th>First Name</th>
+                    <th>Middle Name</th>
+                    <th>Course</th>
+                    <th>Year Level</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    while ($data = mysqli_fetch_assoc($query)) {
+                ?>
+                <tr>
+                    <td><?php echo $data['student_no']; ?></td>
+                    <td><?php echo $data['last_name']; ?></td>
+                    <td><?php echo $data['first_name']; ?></td>
+                    <td><?php echo $data['middle_name']; ?></td>
+                    <td><?php echo $data['course_code']; ?></td>
+                    <td><?php echo $data['year_level']; ?></td>
+                    <td>
+                        <a href="edit.php?student_no=<?php echo $data['student_no']; ?>">Edit</a>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
         </table>
+
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
 
         <script>
-            $(document).ready(function){
-                $('#student_info').DataTables();
-            }
+            $(document).ready(function() {
+                $('#student_info').DataTable();
+            });
         </script>
-        <?php }
-        ?>
+        <?php } ?>
     </body>
 </html>
