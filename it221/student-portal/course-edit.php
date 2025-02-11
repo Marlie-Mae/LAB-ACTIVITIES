49d95bee2242306890bfe6a0ca1d6b171dfe11f1
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2025-02-11 21:15:00 +0800

    Update course-edit.php

--- it221/student-portal/course-edit.php
+++ it221/student-portal/course-edit.php
@@ -2,7 +2,7 @@
     include("cn.php");
     if (isset($_GET['student_no'])) {
         $course_code = mysqli_real_escape_string($connection, $_GET['course_code']);
-        $query = mysqli_query($connection, "SELECT * FROM tbl_course WHERE student_no = '$course_code'");
+        $query = mysqli_query($connection, "SELECT * FROM tbl_course WHERE course_code = '$course_code'");
         $rows = mysqli_num_rows($query);
         if ($rows > 0) {
             $data = mysqli_fetch_assoc($query);
@@ -26,31 +26,9 @@
             <label for="course_code">Course Code:</label>
             <input type="text" name="course_code" placeholder="course_code" required value="<?php if(isset($course_code)) { echo $course_code;} ?>"> <br>
 
-            <label for="l_name">Last Name:</label>
-            <input type="text" name="l_name" placeholder="Last name" required value="<?php if(isset($data['last_name'])) {echo $data["last_name"];} ?>"> <br>
+            <label for="course_name">Course Name:</label>
+            <input type="text" name="course_name" placeholder="Course name" required value="<?php if(isset($data['course_name'])) {echo $data["course_name"];} ?>"> <br>
 
-            <label for="f_name">First Name:</label>
-            <input type="text" name="f_name" placeholder="First name" required value="<?php if(isset($data['first_name'])) {echo $data["first_name"];} ?>"> <br>
-
-            <label for="m_name">Middle Name:</label>
-            <input type="text" name="m_name" placeholder="Middle name" required value="<?php if(isset($data['middle_name'])) {echo $data["middle_name"];} ?>"> <br>
-
-            <label for="course_code">Course:</label>
-            <select name="course_code" id="">
-                <option value="" disabled selected>Select a course</option>
-                <?php
-                $course_query = mysqli_query($connection, "SELECT * FROM tbl_course"); 
-                while ($course_row = mysqli_fetch_assoc($course_query)) {
-                    $selected = ($course_row['course_code'] == $data['course_code']) ? "selected" : "";
-                    echo "<option value='{$course_row['course_code']}' $selected>{$course_row['course_code']}</option>";
-                }
-                ?>
-            </select>
-            <br>
-
-            <label for="year_level">Year Level:</label>
-            <input type="number" name="year_level" placeholder="Year level" min="1" max="4" required value="<?php if(isset($data['year_level'])) {echo $data["year_level"];} ?>"> <br>
-            
             <input type="submit" name="" value="UPDATE">
         </form>
     </body>
