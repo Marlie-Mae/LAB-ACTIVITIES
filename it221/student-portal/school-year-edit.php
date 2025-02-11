732046624e3eb7cf010659ac8942929c1a7360c1
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2025-02-11 21:48:34 +0800

    Create school-year-edit.php

---
+++ it221/student-portal/school-year-edit.php
@@ -0,0 +1,41 @@
+<?php
+    include("cn.php");
+    if (isset($_GET['school_year_code'])) {
+        $school_year_code = mysqli_real_escape_string($connection, $_GET['school_year_code']);
+        $query = mysqli_query($connection, "SELECT * FROM tbl_school_year WHERE school_year_code = '$school_year_code'");
+        $rows = mysqli_num_rows($query);
+        if ($rows > 0) {
+            $data = mysqli_fetch_assoc($query);
+        } else {
+            echo "School Year Code not found";
+        }
+    } else {
+        header("location: school-year.php");
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Edit School Year</title>
+    </head>
+    <body>
+        <form action="" method="POST">
+            <label for="school_year_code">School Year Code:</label>
+            <input type="text" name="school_year_code" placeholder="School year code" required value="<?php if(isset($school_year_code)) { echo $school_year_code;} ?>"> <br>
+
+            <label for="school_year">School Year:</label>
+            <input type="text" name="school_year" placeholder="School year" required value="<?php if(isset($data['school_year'])) {echo $data["school_year"];} ?>"> <br>
+
+            <label for="semester">Semester:</label>
+            <input type="text" name="semester" placeholder="Semester" required value="<?php if(isset($data['semester'])) {echo $data["semester"];} ?>"> <br>
+
+            <label for="status">Status:</label>
+            <input type="text" name="status" placeholder="Status" required value="<?php if(isset($data['status'])) {echo $data["status"];} ?>"> <br>
+
+            <input type="submit" name="" value="UPDATE">
+        </form>
+    </body>
+</html>
