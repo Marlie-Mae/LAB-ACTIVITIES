87b07adbfeceefac96388d574a87dbd66072ea3f
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2025-02-15 23:09:29 +0800

    Update faculty-edit.php

--- it221/student-portal/faculty-edit.php
+++ it221/student-portal/faculty-edit.php
@@ -6,12 +6,32 @@
         $rows = mysqli_num_rows($query);
         if ($rows > 0) {
             $data = mysqli_fetch_assoc($query);
+            $department_code = $data['department_code'];
         } else {
             echo "Faculty Code not found";
         }
     } else {
         header("location: faculty.php");
     }
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $faculty_code = mysqli_real_escape_string($connection, $_POST['faculty_code']);
+        $faculty_name = mysqli_real_escape_string($connection, $_POST['faculty_name']);
+        $department_code = mysqli_real_escape_string($connection, $_POST['department_code']);
+
+        $check_faculty = mysqli_query($connection, "SELECT * FROM tbl_faculty WHERE faculty_code = '$faculty_code'");
+        $rows = mysqli_num_rows($check_faculty);
+        if ($rows == 1) {
+            $sql = "UPDATE tbl_faculty SET faculty_name = '$faculty_name', department_code = '$department_code' WHERE faculty_code = '$faculty_code'";
+            if (mysqli_query($connection,$sql)) {
+                header("location: faculty.php");
+            } else {
+                $display = "Failed to update records.";
+            }
+        } else {
+            $display = "Faculty Code not found.";
+        }
+    } 
 ?>
 
 <!DOCTYPE html>
@@ -22,9 +42,11 @@
         <title>Edit Faculty</title>
     </head>
     <body>
+        <?php if(isset($status)) {echo $status; } ?>
+        <?php if(isset($display)) {echo $display; } ?>
         <form action="" method="POST">
             <label for="faculty_code">Faculty Code:</label>
-            <input type="text" name="faculty_code" placeholder="Faculty code" required value="<?php if(isset($faculty_code)) { echo $faculty_code;} ?>"> <br>
+            <input type="text" name="faculty_code" placeholder="Faculty Code" required value="<?php if(isset($faculty_code)) { echo $faculty_code;} ?>"> <br>
 
             <label for="faculty_name">Faculty Name:</label>
             <input type="text" name="faculty_name" placeholder="Faculty name" required value="<?php if(isset($data['faculty_name'])) {echo $data["faculty_name"];} ?>"> <br>
@@ -33,11 +55,14 @@
             <select name="department_code" id="">
                 <option value="" disabled selected>Select a department</option>
                 <?php
-                $department_query = mysqli_query($connection, "SELECT * FROM tbl_department"); 
-                while ($department_row = mysqli_fetch_assoc($department_query)) {
-                    $selected = ($department_row['department_code'] == $data['department_code']) ? "selected" : "";
-                    echo "<option value='{$department_row['department_code']}' $selected>{$department_row['department_code']}</option>";
-                }
+                $query = mysqli_query($connection, "SELECT * FROM tbl_department"); 
+                $rows = mysqli_num_rows($query);
+                while ($department = mysqli_fetch_assoc($query)) {
+                ?>
+                <option value="<?php echo $department['department_code']; ?>" <?php if($department_code == $department["department_code"]) {echo "selected"; }?>>
+
+                <?php echo $department['department_code']; ?> </option>
+                <?php } ?>
                 ?>
             </select>
             <br>
