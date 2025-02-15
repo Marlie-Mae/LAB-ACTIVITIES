59f97cdcd2e347cdb1ecdb6f49a06ffbf6ff905c
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2025-02-15 22:59:35 +0800

    Update department-edit.php

--- it221/student-portal/department-edit.php
+++ it221/student-portal/department-edit.php
@@ -12,6 +12,24 @@
     } else {
         header("location: department.php");
     }
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $department_code = mysqli_real_escape_string($connection, $_POST['department_code']);
+        $department_name = mysqli_real_escape_string($connection, $_POST['department_name']);
+
+        $check_department = mysqli_query($connection, "SELECT * FROM tbl_department WHERE department_code = '$department_code'");
+        $rows = mysqli_num_rows($check_department);
+        if ($rows == 1) {
+            $sql = "UPDATE tbl_department SET department_name = '$department_name' WHERE department_code = '$department_code'";
+            if (mysqli_query($connection,$sql)) {
+                header("location: department.php");
+            } else {
+                $display = "Failed to update records.";
+            }
+        } else {
+            $display = "Department Code not found.";
+        }
+    } 
 ?>
 
 <!DOCTYPE html>
@@ -22,9 +40,11 @@
         <title>Edit Department</title>
     </head>
     <body>
+        <?php if(isset($status)) {echo $status; } ?>
+        <?php if(isset($display)) {echo $display; } ?>
         <form action="" method="POST">
             <label for="department_code">Department Code:</label>
-            <input type="text" name="department_code" placeholder="department_code" required value="<?php if(isset($department_code)) { echo $department_code;} ?>"> <br>
+            <input type="text" name="department_code" placeholder="Department code" required value="<?php if(isset($department_code)) { echo $department_code;} ?>"> <br>
 
             <label for="department_name">Department Name:</label>
             <input type="text" name="department_name" placeholder="Department name" required value="<?php if(isset($data['department_name'])) {echo $data["department_name"];} ?>"> <br>
