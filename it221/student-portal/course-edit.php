fa4e75dd9b28512c1948fe685f1192e8664dd9e0
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2025-02-15 22:44:55 +0800

    Update course-edit.php

--- it221/student-portal/course-edit.php
+++ it221/student-portal/course-edit.php
@@ -12,6 +12,24 @@
     } else {
         header("location: course.php");
     }
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $course_code = mysqli_real_escape_string($connection, $_POST['course_code']);
+        $course_description = mysqli_real_escape_string($connection, $_POST['course_description']);
+
+        $check_course = mysqli_query($connection, "SELECT * FROM tbl_course WHERE course_code = '$course_code'");
+        $rows = mysqli_num_rows($check_course);
+        if ($rows == 1) {
+            $sql = "UPDATE tbl_course SET course_description = '$course_description' WHERE course_code = '$course_code'";
+            if (mysqli_query($connection,$sql)) {
+                header("location: course.php");
+            } else {
+                $display = "Failed to update records.";
+            }
+        } else {
+            $display = "Course Code not found.";
+        }
+    } 
 ?>
 
 <!DOCTYPE html>
@@ -19,15 +37,17 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Edit Course</title>
+        <title>Edit Student</title>
     </head>
     <body>
+        <?php if(isset($status)) {echo $status; } ?>
+        <?php if(isset($display)) {echo $display; } ?>
         <form action="" method="POST">
             <label for="course_code">Course Code:</label>
-            <input type="text" name="course_code" placeholder="course_code" required value="<?php if(isset($course_code)) { echo $course_code;} ?>"> <br>
+            <input type="text" name="course_code" placeholder="Course code" required value="<?php if(isset($course_code)) { echo $course_code;} ?>"> <br>
 
             <label for="course_description">Course Description:</label>
-            <input type="text" name="course_description" placeholder="Course description" required value="<?php if(isset($data['course_description'])) {echo $data["course_description"];} ?>"> <br>
+            <input type="text" name="course_description" placeholder="Course Description" required value="<?php if(isset($data['course_description'])) {echo $data["course_description"];} ?>"> <br>
 
             <input type="submit" name="" value="UPDATE">
         </form>
