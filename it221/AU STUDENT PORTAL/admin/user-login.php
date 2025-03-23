aa1f3d0af0bde7e27229b88a4c3f4d3a5d2c4729
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2025-03-23 19:26:07 +0800

    Add files via upload

---
+++ it221/AU STUDENT PORTAL/admin/user-login.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arellano Univeristy | Login</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="container">
+        <div class="right-panel">
+            <img src="images/au_logo.png" alt="Arellano University Logo" class="seal">
+            <h2>Hi, Chiefs!</h2>
+            <p>Please click or tap your destination.</p>
+            <a href="student-login.php" class="btn student">Student</a>
+            <a href="faculty-login.php" class="btn faculty">Faculty</a>
+            <p class="terms">By using this service, you understood and agree to the Arellano University Online Services <a href="#">Terms of Use</a> and <a href="#">Privacy Statement</a>.</p>
+        </div>
+    </div>
+</body>
+</html>
